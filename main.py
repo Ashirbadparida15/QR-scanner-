84cc213faad8bca28043095d6754905ada97b93b
Ashirbad Parida <ashirbadparida15@gmail.com>
2022-07-28 23:18:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from pyzbar.pyzbar import decode
 
 
 def decoder(image):
-    gray_img = cv2.cvtColor(image, 0)
-    barcode = decode(gray_img)
+    img = cv2.cvtColor(image, 0)
+    barcode = decode(img)
 
-    for obj in barcode:
-        points = obj.polygon
-        (x, y, w, h) = obj.rect
+    for puzzel in barcode:
+        points = puzzel.polygon
+        (x, y, width, hight) = puzzel.rect
         pts = np.array(points, np.int32)
         pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(image, [pts], True, (0, 255, 0), 3)
+        cv2.polylines(image, [pts], True, (0, 0, 255), 3)
 
-        barcodeData = obj.data.decode("utf-8")
-        barcodeType = obj.type
-        string = "Data " + str(barcodeData) + " | Type " + str(barcodeType)
+        myData = puzzel.data.decode("utf-8")
+        barcodeType =puzzel.type
+        string = "Data " + str( myData ) + " | Type " + str(barcodeType)
 
-        cv2.putText(frame, string, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-        print("Barcode: " + barcodeData + " | Type: " + barcodeType)
+        cv2.putText(frame, string, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        print("Barcode: " +  myData  + " | Type: " + barcodeType)
 
 
 cap = cv2.VideoCapture(0)
@@ -27,6 +27,6 @@ while True:
     ret, frame = cap.read()
     decoder(frame)
     cv2.imshow('Image', frame)
-    code = cv2.waitKey(10)
+    code = cv2.waitKey(1)
     if code == ord('q'):
         break
